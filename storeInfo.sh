1d31463851b4601c4d70648a2afab632956b82e0
ArtimosPenguidor <nick13690@yahoo.com>
2014-06-23 14:34:04 -0500

    Delete storeInfo.sh

--- storeInfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gateway=`route -n | grep 'UG[ \t]' | awk '{print $2}'`
-while true; do
-	nmap -sP $gateway/24 | tr -d '()' | grep -vw "Host\|Starting\|done" > tempInfo.txt
-	cat tempInfo.txt > info.txt
-done
